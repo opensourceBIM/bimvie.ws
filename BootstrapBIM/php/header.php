9ea7d5b690adb2ef0a1067db8e07e052cf9828cf
Ruben de Laat <ruben@logic-labs.nl>
2012-10-29 15:34:57 +0100

    - Removed PHP folder (moved to another repo)

--- BootstrapBIM/php/header.php
+++
@@ -1,9 +0,0 @@
-<?
-	$host = "localhost";
-	$database = "bimservernotifications";
-	$username = "bimser";
-	$password = "PxcYzzbqKLHUwGuH";
-	
-	mysql_connect($host, $username, $password);
-	mysql_select_db($database);
-?>
