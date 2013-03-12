f747b90083d0c57a7c4c9e5827443d4507debd6a
Ruben de Laat <ruben@logic-labs.nl>
2013-03-12 22:33:11 +0100

    Changes for Admin GUI

--- BootstrapBIM/js/settings.js
+++ BootstrapBIM/js/settings.js
@@ -4,5 +4,16 @@ var Settings = {
 			console.log(data);
 			callback(data.servers);
 		}).error(function(error) {console.log(error); });
+	},
+	getMenuItems: function(){
+		return [
+		        "dashboardLink",
+		        "projectsLink",
+		        "usersLink",
+		        "usersettingsLink",
+		        "serversettingsLink",
+		        "serverinfoLink",
+		        "testingLink"
+		];
 	}
 }
