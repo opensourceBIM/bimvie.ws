d7ddc302b2fd94d576d8793a0b2ed6af5108e4b1
Ruben <ruben@logic-labs.nl>
2013-05-06 15:11:14 +0200

    Links to BIMsurfer

--- BootstrapBIM/js/settings.js
+++ BootstrapBIM/js/settings.js
@@ -1,19 +1,21 @@
 var Settings = {
 	getServerList: function(callback){
 		$.getJSON("http://extend.bimserver.org/serverlist", function(data, textStatus, jqXHR){
-			console.log(data);
 			callback(data.servers);
 		}).error(function(error) {console.log(error); });
 	},
+	get3DViewerUrl: function(bimServerApi, roid){
+		return "http://localhost:8080/bimsurfer/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
+	},
 	getMenuItems: function(){
 		return [
-		        "dashboardLink",
-		        "projectsLink",
-		        "usersLink",
-		        "usersettingsLink",
-		        "serversettingsLink",
-		        "serverinfoLink",
-		        "testingLink"
+	        "dashboardLink",
+	        "projectsLink",
+	        "usersLink",
+	        "usersettingsLink",
+	        "serversettingsLink",
+	        "serverinfoLink",
+	        "testingLink"
 		];
 	}
 }
