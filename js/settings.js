6ed01a64473efc56e1b474158754bf1bdd2cc531
Ruben <ruben@logic-labs.nl>
2013-09-09 11:40:50 +0200

    http://test.bimsurfer.org/

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,6 @@
 var Settings = {
-	getServerList: function(callback){
-		$.getJSON("http://extend.bimserver.org/serverlist", function(data, textStatus, jqXHR){
-			callback(data.servers);
-		}).error(function(error) {console.log(error); });
-	},
 	get3DViewerUrl: function(bimServerApi, roid){
-		return "http://localhost:8080/bimsurfer/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
+		return "http://test.bimsurfer.org/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
 	},
 	getMenuItems: function(){
 		return [
