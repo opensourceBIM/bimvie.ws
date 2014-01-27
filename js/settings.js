01272db3a0d7363befbe2776cce9b992b1da9f17
Ruben de Laat <ruben@logic-labs.nl>
2014-01-27 17:04:34 +0100

    Added ability to set smtp server to remote server

--- js/settings.js
+++ js/settings.js
@@ -1,6 +1,6 @@
 var Settings = {
 	get3DViewerUrl: function(bimServerApi, roid){
-		return "http://localhost:8080/bimsurfer/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
+		return "http://test.bimsurfer.org/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
 	},
 	getMenuItems: function(){
 		return [
