a5ac87e94a33d653b5738d67f48baf2a3cd99b15
Ruben de Laat <ruben@logic-labs.nl>
2015-04-19 14:55:23 +0200

    Made tree faster

--- js/tree.js
+++ js/tree.js
@@ -4,6 +4,7 @@ function Tree(selector) {
 	o.idToNodeMap = {};
 	o.rootNode = Object.create(Node.prototype);
 	o.rootNode.init(-1);
+	o.rootNode.open();
 	o.selectedNode = null;
 	o.selectionListeners = [];
 
@@ -36,37 +37,10 @@ function Tree(selector) {
 
 function Node() {};
 
-var x = $("<li class=\"treenode\"><div class=\"treeicon\"></div><div class=\"treepreicon\"></div><div class=\"treelabel\"><a></a></div></li>");
-
 Node.prototype.init = function(id, title){
 	this.isOpen = false;
 	this.id = id;
 	this.title = title;
-
-	this.li = x.clone();
-	this.li.find(".treenode").attr("nodeid", id);
-	this.li.find("a").html(title);
-	this.li.find(".treeicon").click(this.toggle.bind(this));
-	this.img = this.li.find(".treeicon");
-	this.a = this.li.find(".treelabel a");
-	this.icon = this.li.find(".treepreicon");
-	
-//	this.li = $("<li class=\"treenode\">");
-//	this.li.attr("nodeid", id);
-//	this.div = $("<div class=\"treelabel\">");
-//	this.a = $("<a>" + title + "</a>");
-//	this.img = $("<div class=\"treeicon\"/>");
-//	this.img.click(this.toggle.bind(this));
-//	this.li.append(this.img);
-//	this.icon = $("<div class=\"treepreicon\">");
-//	this.li.append(this.icon);
-//	this.div.append(this.a);
-//	this.li.append(this.div);
-//	this.ul = $("<ul>");
-//	this.ul.hide();
-//	this.li.append(this.ul);
-
-	this.a.click(this.linkClick.bind(this));
 };
 
 Node.prototype.linkClick = function(){
@@ -93,8 +67,9 @@ Node.prototype.add = function(node){
 	if (this.children == null) {
 		this.children = [];
 	}
-	if (this.ul == null) {
-		this.ul = $("<ul>");
+	this.createDom();
+	if (this.isOpen) {
+		node.createDom();
 	}
 	if (this.sort) {
 		var found = false;
@@ -128,22 +103,100 @@ Node.prototype.add = function(node){
 };
 
 Node.prototype.setIcon = function(icon){
-	this.icon.addClass(icon);
+	this.iconclass = icon;
+	if (this.icon != null) {
+		this.icon.addClass(this.iconclass);
+	}
 };
 
 Node.prototype.onLoad = function(listener){
 	this.getOnLoadListeners().register(listener);
-	this.img.addClass("treenodeclosed");
+	if (this.img != null) {
+		this.img.addClass("treenodeclosed");
+	}
 };
 
 Node.prototype.click = function(click){
 	this.onClick = click;
 };
 
-Node.prototype.open = function(){
+Node.prototype.createDom = function(){
+	if (this.li == null) {
+		this.li = $("<li class=\"treenode\">");
+		this.li.attr("nodeid", this.id);
+		this.div = $("<div class=\"treelabel\">");
+		this.a = $("<a>" + this.title + "</a>");
+		this.img = $("<div class=\"treeicon\"/>");
+		this.img.click(this.toggle.bind(this));
+		this.li.append(this.img);
+		this.icon = $("<div class=\"treepreicon\">");
+		this.li.append(this.icon);
+		this.div.append(this.a);
+		this.li.append(this.div);
+		this.ul = $("<ul>");
+		this.ul.hide();
+		this.li.append(this.ul);
+		this.a.click(this.linkClick.bind(this));
+		
+		if (this.hint != null) {
+			this.a.attr("title", this.hint);
+		}
+		
+		if (this.button != null) {
+			this.li.find(".treelabel").after(this.button);
+		}
+
+		if (this.iconclass != null) {
+			this.icon.addClass(this.iconclass);
+		}
+		
+		if (this.parent != null) {
+			this.parent.createDom();
+			this.parent.ul.append(this.li);
+		}
+		
+		this.img.addClass("treenodeclosed");
+	}
+};
+
+Node.prototype.loadRecursiveSimple = function(){
+	var promise = new Promise();
+	if (this.onLoadListeners != null && this.onLoadListeners.size() > 0) {
+		this.onLoadListeners.trigger(function(listener){
+			promise.chain(listener());
+		});
+		this.onLoadListeners.clear();
+	} else {
+		promise.fire();
+	}
+	var o = this;
+	var newPromise = new Promise();
+	promise.done(function(){
+		if (o.children != null) {
+			newPromise.chain(o.children.map(function(child){
+				return child.loadRecursiveSimple();
+			}));
+		} else {
+			newPromise.fire();
+		}
+	});
+	return newPromise;
+};
+
+Node.prototype.loadRecursively = function(){
 	var promise = new Promise();
 	this.setLoading();
-	this.isOpen = true;
+	var o = this;
+	this.loadRecursiveSimple().done(function(){
+		o.doneLoading();
+		promise.fire();
+	});
+	return promise;
+};
+
+Node.prototype.load = function(){
+	var promise = new Promise();
+	this.setLoading();
 	if (this.onLoadListeners != null && this.onLoadListeners.size() > 0) {
 		this.onLoadListeners.trigger(function(listener){
 			promise.chain(listener());
@@ -154,18 +207,30 @@ Node.prototype.open = function(){
 	}
 	var o = this;
 	promise.done(function(){
+		o.doneLoading();
+	});
+	return promise;
+};
+
+Node.prototype.open = function(){
+	this.isOpen = true;
+	this.createDom();
+	var promise = this.load();
+	var o = this;
+	promise.done(function(){
+		if (o.children != null && o.children.length > 0) {
+			o.children.forEach(function(child){
+				child.createDom();
+			});
+			o.img.removeClass("treenodeclosed");
+			o.img.addClass("treenodeopen");
+		}
 		if (o.ul != null) {
 			if (o.li.find("ul").length == 0) {
 				o.li.append(o.ul);
 			} else {
 				o.ul.show();
 			}
-		}
-		if (o.children != null && o.children.length > 0) {
-			o.img.removeClass("treenodeclosed");
-			o.img.addClass("treenodeopen");
-		} else {
-			o.doneLoading();
 		}
 	});
 	return promise;
@@ -193,6 +258,7 @@ Node.prototype.select = function(){
 	if (this.tree.selectedNode != null) {
 		this.tree.selectedNode.unselect();
 	}
+	this.createDom();
 	this.tree.setSelectedNode(this);
 	this.li.addClass("selected");
 };
@@ -203,7 +269,10 @@ Node.prototype.unselect = function(){
 };
 
 Node.prototype.hint = function(hint) {
-	this.a.attr("title", hint);
+	this.hint = hint;
+	if (this.a != null) {
+		this.a.attr("title", hint);
+	}
 };
 
 Node.prototype.setTitle = function(title){
@@ -215,20 +284,24 @@ Node.prototype.hide = function(){
 };
 
 Node.prototype.setLoading = function(){
-	this.img.removeClass("treenodeopen");
-	this.img.removeClass("treenodeclosed");
-	this.img.addClass("loading");
+	if (this.img != null) {
+		this.img.removeClass("treenodeopen");
+		this.img.removeClass("treenodeclosed");
+		this.img.addClass("loading");
+	}
 };
 
 Node.prototype.doneLoading = function(){
-	this.img.removeClass("loading");
-	if (this.children != null && this.children.length > 0) {
-		if (this.isOpen) {
-			this.img.addClass("treenodeopen");
-		} else {
-			this.img.addClass("treenodeclosed");
-		}
-	}			
+	if (this.img != null) {
+		this.img.removeClass("loading");
+		if (this.children != null && this.children.length > 0) {
+			if (this.isOpen) {
+				this.img.addClass("treenodeopen");
+			} else {
+				this.img.addClass("treenodeclosed");
+			}
+		}			
+	}
 };
 
 Node.prototype.toggle = function(){
@@ -293,5 +366,8 @@ Node.prototype.internalListFilterByType = function(result, type){
 };
 
 Node.prototype.addButton = function(button){
-	this.li.find(".treelabel").after(button);
+	this.button = button;
+	if (this.li != null) {
+		this.li.find(".treelabel").after(this.button);
+	}
 };
