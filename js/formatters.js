1faa080637c22a4eecec17455d4b029e66aafb09
Ruben de Laat <ruben@logic-labs.nl>
2015-06-01 12:24:48 +0200

    test

--- js/formatters.js
+++ js/formatters.js
@@ -7,12 +7,12 @@ function GetHumanSize(size) {
 }
 
 function bytesToSize(bytes) {
-	   if(bytes == 0) return '0 Byte';
-	   var k = 1000;
-	   var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-	   var i = Math.floor(Math.log(bytes) / Math.log(k));
-	   return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
-	}
+	if(bytes == 0) return '0 Byte';
+	var k = 1000;
+	var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+	var i = Math.floor(Math.log(bytes) / Math.log(k));
+	return (bytes / Math.pow(k, i)).toPrecision(3) + ' ' + sizes[i];
+}
 
 function formatDate(date) {
 	if (date == null) {
