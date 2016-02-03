df3858969a6d9eed80730b21ad5b7af5c06a8231
Ruben de Laat <ruben@logic-labs.nl>
2016-02-03 13:11:47 +0100

    - working on regenerate geometry menu - see revision oid in tooltip - plugins

---
+++ build/settings.js
@@ -0,0 +1,83 @@
+var Settings = {
+	get3DViewerUrl: function(bimServerApi, roid){
+		return "http://test.bimsurfer.org/index.html?token=" + bimServerApi.token + "&roid=" + roid + "&server=" + bimServerApi.baseUrl;
+	},
+	createStartPage: function(container, main){
+		pushHistory({page: "Projects"}, "Projects");
+		main.pageChanger.changePage($(".serverinfoLink"), "projects.html", function(){
+			return new Projects($(this), main);
+		});
+	},
+	getPlugins: function(){
+		return {
+			relatics: {
+				enabled: false
+			}
+		};
+	},
+	getTitle: function(){
+		return "BIMvie.ws";
+	},
+	useBimSurfer: function(){
+		return true;
+	},
+	getMenuItems: function(){
+		return [
+	        "dashboardLink",
+	        "userLink",
+	        "projectLink",
+	        "usersettingsLink",
+	        "serversettingsLink",
+	        "gettingStartedLink",
+	        "userDivider",
+	        "btnAddUser",
+	        "btnListUsers"
+		];
+	},
+	getVersion: function(successCallback){
+		// Sends back the version to the successCallback, default case would be to assume BIMserver to be running on the same webserver (hence asking for the plugin.version resource)
+		// When running BIMvie.ws on a different server, replace this with a callback with a fixed version
+		// This version is usually used for the invalidation of the caching of BIMvie.ws resources
+
+		var myRequest = new XMLHttpRequest();
+		myRequest.onreadystatechange = function() {
+			if (myRequest.readyState != 4)  {
+				// We are waiting for a readyState 4 here
+				return;
+			}
+			if (myRequest.status != 200)  {
+				successCallback(new Date().getTime());
+				return;
+			} else {
+				successCallback(JSON.parse(myRequest.responseText).version);
+			}
+		};
+		myRequest.open("GET", Global.baseDir + "plugin.version", true);
+		myRequest.send();
+	},
+	usableBimServerVersion: function(version) {
+		return (version.major == 1 && version.minor == 3) || (version.major == 1 && version.minor == 4);
+	},
+	getStaticServerAddress: function(callback){
+		$.getJSON("x.getbimserveraddress", function(data){
+			callback(data.address);
+		}).fail(function(){
+			callback(null);
+		});
+	},
+	useCompressedResources: function(){
+		return true;
+	},
+	allowBimServerAddress: function() {
+		return false;
+	},
+	getBimServerApiAddress: function(){
+		return "../javascriptapi/js";
+	},
+	getBimSurferApiAddress: function(){
+		return "../bimsurfer/";
+	},
+	getDefaultHiddenTypes: function() {
+		return {"IfcOpeningElement": true, "IfcSpace": true};
+	}
+}
