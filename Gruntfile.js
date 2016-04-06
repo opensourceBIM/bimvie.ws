3cc4b1c42c6b670c5ac618c80e549e769ee9e286
Ruben de Laat <ruben@logic-labs.nl>
2016-04-06 10:19:30 +0200

    removed github releaser

--- Gruntfile.js
+++ Gruntfile.js
@@ -74,29 +74,9 @@ module.exports = function(grunt) {
 						src: ["output/**"],
 						dest: "output/bimviews-%VERSION%.zip"
 					}
-				},
-				"github-release" : {
-					options : {
-						repository : "opensourceBIM/bimvie.ws",
-						auth : {
-							user : "%USERNAME%",
-							password : "%PASSWORD%"
-						},
-						release : {
-							tag_name : "%VERSION%",
-							name : "BIMvie.ws %VERSION%",
-							body : "Testing...",
-							draft : false,
-							prerelease : true
-						}
-					},
-					files : {
-						src : [ "output/bimviews-%VERSION%.zip" ]
-					}
 				}
 			});
 
-	grunt.loadNpmTasks("grunt-github-releaser");
 	grunt.loadNpmTasks("grunt-contrib-concat");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks("grunt-contrib-cssmin");
