9d042136fa1a5a74c517508de51aa25aaab8388d
hlg <public.void@gmx.de>
2024-08-28 16:58:45 +0200

    fix BIMvie.ws running on a different server from BIMServer
    return timestamp as a string for the BIMvie.ws version

--- js/settings.js
+++ js/settings.js
@@ -43,7 +43,7 @@ var Settings = {
 				return;
 			}
 			if (myRequest.status != 200)  {
-				successCallback(new Date().getTime());
+				successCallback(new Date().getTime().toString());
 				return;
 			} else {
 				successCallback(JSON.parse(myRequest.responseText).version);
